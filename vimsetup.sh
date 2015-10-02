7579c62d9e382072f04a914228aed0f3568f6c14
Xin Wang <wangxin@quizor1.cs.pdx.edu>
2015-10-02 08:53:59 -0700

    Now install Vundle at the begining

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #replace .vimrc file and install solarized colorscheme
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 cp -r Vundle.vim ~/.vim/bundle/
 cp .vimrc ~/.vimrc
 cp solarized.vim ~/.vim/colors/
